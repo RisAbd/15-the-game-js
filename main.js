092e11770d78a6b6eb281cfc50232c793637e892
Abdullah Ibragimov <irsalabd@gmail.com>
2020-04-16 02:20:16 +0600

    add ui, add css, add controls

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 
 class Game extends Array {
   constructor(w, h) {
+    if (h === undefined) {
+      // when using .map it calls new Game(16)
+      super(w);
+      return;
+    }
+    
     super(w*h);
     this.w = w; 
     this.h = h;
@@ -25,7 +31,7 @@ class Game extends Array {
       return pos;
     } 
     // convert to flat position index
-    return this._xy(pos);
+    return this._flatPos(pos);
   }
   _ensureXY(pos) {
     this._xy(this._ensureFlat(pos));
@@ -43,14 +49,32 @@ class Game extends Array {
   get(pos) {
     return this[this._ensureFlat(pos)];
   }
+
+  /**
+    * return [pos, newPos] for moved cell if any, else return undefined
+    *
+  **/
   moveDirection(dir) {
+    const [x, y] = this._xy(this.map((_, i) => i).filter(pos => (this[pos] === 0))[0]);  // get position of empty cell
     switch (dir) {
-      case 'up': 
-      case 'bottom':
-      case 'right':
-      case 'left':
+      case 'up': {
+        if (y+1 >= this.h) return;  // impossible move
+        return this.moveCell([x, y+1])
+      }
+      case 'down': {
+        if (y-1 < 0) return;
+        return this.moveCell([x, y-1]);
+      }
+      case 'right': {
+        if (x-1 < 0) return;
+        return this.moveCell([x-1, y]);
+      }
+      case 'left': {
+        if (x+1 >= this.w) return;
+        return this.moveCell([x+1, y]);
+      }
       default: {
-        throw new Error(`unknwon move direction: ${dir}`);
+        throw new Error(`unknown move direction: ${dir}`);
       }
     }
   }
@@ -58,6 +82,7 @@ class Game extends Array {
   /**
     * return [pos, newPos] of new cell if cell with given position has empty adjacent cell
     * or return undefined if move can not be done
+    *
   **/
   moveCell(pos) {
     pos = this._ensureFlat(pos);
@@ -75,27 +100,82 @@ class Game extends Array {
       .filter(([x, y]) => (x >= 0 && x < this.w && y >= 0 && y < this.h))   // leave only possible values
       .map(this._flatPos.bind(this));
 
-    console.log(adjacentPositions);
-
     const [newPos] = adjacentPositions
       .filter(pos => this[pos] === 0);     // leave only one with empty cell
-    
-    console.log(newPos);
 
     // newPos contains zero value === empty cell
     if (newPos !== undefined) {
       // swap values
       [this[newPos], this[pos]] = [this[pos], this[newPos]];
-      // return
+      // todo: check if game is ended
       return [pos, newPos];
     }
   }
 }
 
 const game = new Game(4, 4).shuffle();
-console.log(''+game);
-
 
 const gameContainer = document.querySelector('#game-container');
+const colors = ['red', 'blue', 'green', 'purple'];
+
+const cells = new Array(4*4).fill(0).map((_, i) => {
+  const value = game[i];
+  const cell = document.createElement('button');
+  cell.classList.add('cell');
+  cell.innerText = value;
+  cell.dataset.value = value;
+  if (value === 0) { cell.tabIndex = -1; }
+  cell.addEventListener('click', onCellClick);
+
+  cell.style.color = colors[i%colors.length];
+
+  gameContainer.appendChild(cell);  
+
+  return cell;
+});
+
+function swapCells(fromPos, toPos) {
+  const fromCell = gameContainer.children[fromPos];
+  const toCell = gameContainer.children[toPos];
+  swapElements(fromCell, toCell);
+}
+
+function swapElements(obj1, obj2) {
+  var temp = document.createElement("div");
+  obj1.parentNode.insertBefore(temp, obj1);
+  obj2.parentNode.insertBefore(obj1, obj2);
+  temp.parentNode.insertBefore(obj2, temp);
+  temp.parentNode.removeChild(temp);
+}
 
 
+// control by cell click
+function onCellClick(e) {
+  const pos = Array.prototype.indexOf.call(gameContainer.children, e.target);
+  const res = game.moveCell(pos);
+  if (res) {
+    swapCells(...res);
+  }
+}
+
+
+// control by key
+
+const keyCodeDirection = {
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  37: 'left',
+};
+
+function onKeyDown(e) {
+  if (keyCodeDirection.hasOwnProperty(e.keyCode)) {
+    const direction = keyCodeDirection[e.keyCode];
+    const res = game.moveDirection(direction);
+    if (res) {
+      swapCells(...res);
+    }
+  }
+}
+
+document.addEventListener('keydown', onKeyDown);
